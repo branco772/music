ac7a06f0db6f87b9ea0ca548faec852f7c851dbd
branco772 <eduararratia@gmail.com>
2023-11-09 16:38:16 -0400

    arreglo de navbar

--- components/navBar.jsx
+++ components/navBar.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { useState } from "react";
 
 function NavBar(){
-    const [navbar, setNavBar]=useState("");
+    const [navbar, setNavBar]=useState(false);
     return(
         <div>
             <nav className="w-full bg-black fixed top-0 left-0 right-0 z-10">
@@ -19,9 +19,9 @@ function NavBar(){
                             <div className="md:hidden">
                                 <button className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border" onClick={()=> setNavBar(!navbar)}>
                                     {navbar ? (
-                                        <Image src="/public/close.svg" width={30} height={30} alt="logo"/>
+                                        <Image src={close} width={30} height={30} alt="logo"/>
                                     ):(
-                                        <Image src="/public/hamburger-menu.svg/" width={30} height={30} alt="logo" className="focus:border-none active:border-none"/>
+                                        <Image src="/public/hamburger-menu.svg" width={30} height={30} alt="logo" className="focus:border-none active:border-none"/>
                                     )}
                                 </button>
                             </div>
@@ -32,17 +32,17 @@ function NavBar(){
                             className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? 'p-12 md:p-0 block' : 'hidden'}`}>
                             <ul className="h-screen md:h-auto items-center justify-center md:flex">
                                 <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                                    <Link href="Inicio" onClick={() => setNavbar(!navbar)}>
+                                    <Link href="#inicio" onClick={()=>setNavBar(!navbar)}>
                                         Inicio
                                     </Link>
                                 </li>
                                 <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                                    <Link href="tienda" onClick={() => setNavbar(!navbar)}>
+                                    <Link href="#tienda" onClick={() => setNavBar(!navbar)}>
                                         Tienda
                                     </Link>
                                 </li>
                                 <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                                    <Link href="miPerfil" onClick={() => setNavbar(!navbar)}>
+                                    <Link href="#miPerfil" onClick={() => setNavBar(!navbar)}>
                                         Mi Perfil
                                     </Link>
                                 </li>
