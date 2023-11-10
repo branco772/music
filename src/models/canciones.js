4163b7e18c6baeb2a19fdd66f354d3093f87ab77
branco772 <eduararratia@gmail.com>
2023-11-10 19:39:55 -0400

    esquema canciones

---
+++ src/models/canciones.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+const { Schema,model,models } = mongoose;
+const cancionSchema = new Schema({
+  titulo:{
+    type:String,
+    unique:true,
+    trim:true,
+    required:[true,"El titulo es obligatorio"]
+  },
+  imagen:{
+    type:String,
+    trim:true,
+    required:[true,"Imagen Obligatoria"],
+    default:'#'
+  },
+  artista:{
+    type:String,
+    trim:true,
+    required:[true,"El nombre del artista es obligatorio"]
+  },
+  visible:{
+    type:Boolean,
+    default:true
+  },
+  orden:{
+    type:Number,
+    default:0
+  }
+},
+{
+    timestamps:true
+}
+);
+
+export default models.Cancion || model('Cancion',cancionSchema);  
