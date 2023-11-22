5106783cdebfdfe5ec761c8879a8248adeb61f2c
branco772 <eduararratia@gmail.com>
2023-11-21 20:04:59 -0400

    arreglos

--- src/app/tienda/[id]/update/page.jsx
+++ src/app/tienda/[id]/update/page.jsx
@@ -71,12 +71,12 @@ function HomePage ({params}){
     return(
         <div className="h-[calc(100vh-7rem)] flex justify-center items-center">
             <form onSubmit={handlerSubmit}>
-                <input type="text" name="titulo" placeholder="Ingrese Titulo materia"
+                <input type="text" name="titulo" placeholder="Ingrese Titulo de la cancion"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-lg
                     rounded-lg w-full p-4 my-1"
                 onChange={handlerChange} value={newCancion.titulo}/>
 
-                <input type="text" name="artista" placeholder="Ingrese Titulo materia"
+                <input type="text" name="artista" placeholder="Ingrese Titulo del artista"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-lg
                     rounded-lg w-full p-4 my-1"
                 onChange={handlerChange} value={newCancion.artista}/>
@@ -99,7 +99,7 @@ function HomePage ({params}){
                 onChange={handlerChange} value={newCancion.orden}/>
                 <button type="submit"
                 className="mt-3 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-                >Modificar Materia</button>
+                >Modificar Cancion</button>
             </form>
         </div>
     )
